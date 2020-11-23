5f13a53536c4ed16b5f51aa524866365acd651d2
jerryyang0611 <jerry87061100@gmail.com>
2020-11-23 21:37:07 +0800

    MoveZero

---
+++ Week_01/MoveZero.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=283 lang=java
+ *
+ * [283] Move Zeroes
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+说明:
+
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+ */
+class MoveZero {
+	public static void main(String[] args) {
+		System.out.println("Hello im java...");
+		System.out.println("Moving zero.......");
+		
+
+	}
+}
+
+// 1. loop, count , zeros
+// 2. create new array, loop 
+// 3. index 操作
+// 4.
+
