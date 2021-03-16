7a461c27ed0b5b8c5f931fec4ce4fe768486b778
jerryyang0611 <jerry87061100@gmail.com>
2021-03-16 14:40:16 +0800

    plus1.java

--- Week_01/reverseLinkedList.java
+++ Week_01/reverseLinkedList.java
@@ -30,13 +30,11 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null, curr = head;   //前指针节点, //当前指针节点
         while (curr != null) {   //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
-            ListNode next = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
+            ListNode tmp = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
             curr.next = prev; //将当前节点指向它前面的节点
             prev = curr; //前指针后移
-            curr = next; //当前指针后移
+            curr = tmp; //当前指针后移
         }
-        // head = prev;
-        // return head;
         return prev;
     }
 }
