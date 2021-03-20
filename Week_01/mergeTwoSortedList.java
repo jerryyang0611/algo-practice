e42b7eeb99d6a73c927cfe570f3194974d727e54
jerryyang0611 <jerry87061100@gmail.com>
2021-03-20 22:24:07 +0800

    update mergeTwoSortedList.java

--- Week_01/mergeTwoSortedList.java
+++ Week_01/mergeTwoSortedList.java
@@ -53,12 +53,9 @@ class Solution {
         return head.next;
 
         // Option 2: Recursion...
-        // if (l1 == null) {
-        //     return l2;
-        // }
-        // if (l2 == null) {
-        //     return l1;
-        // }
+        // if(l1 == null || l2 == null)
+        //    return l1 == null ? l2 : l1;
+
         // if (l1.val <= l2.val) {
         //     l1.next = mergeTwoLists(l1.next, l2);
         //     return l1;
